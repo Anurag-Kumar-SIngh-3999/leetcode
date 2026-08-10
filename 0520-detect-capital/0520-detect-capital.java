22d8806acdec624cfbbc72aadc4f9e98e2924d29
Anurag-Kumar-SIngh-3999 <anuragks3999@gmail.com>
2026-08-10 22:29:18 +0530

    Time: 1 ms (89.28%), Space: 43.1 MB (78.89%) - LeetHub

--- 0520-detect-capital/0520-detect-capital.java
+++ 0520-detect-capital/0520-detect-capital.java
@@ -1,34 +1,24 @@
 class Solution {
     public boolean detectCapitalUse(String word) {
-        if(word == null && word.isEmpty()){
-            return false;
-        }
-
-        String upt=word.toUpperCase();
-        //if all words are in uppercase 
-        if(word.equals(upt)){
-            return true ;
-        }
-
-        //if all are lowercaase
-         String lwt=word.toLowerCase();
-         
-
-        //for 1st uppercase and others to be lowercase 
-        if(word.charAt(0)==upt.charAt(0)){
-            for(int i=1;i<word.length();i++){
-                if(word.charAt(i)!=lwt.charAt(i)){
-                    return false;
-                }
+        int upper=0;
+        int n=word.length();
+        for(int i=0;i<word.length();i++){
+            if(Character.isUpperCase(word.charAt(i))){
+                upper++;
             }
-        }else{
-            if(word.equals(lwt)){
-                return true;
-            }else {return false;}
         }
+       if(upper==n){
+           return true;
+       }
+       else if(upper==0){
+           return true;
+       }
+       else if(upper==1 && Character.isUpperCase(word.charAt(0))){
+           return true;
+       }
+       else return false ;
 
-
-        return true;
+        
 
     }
 }
