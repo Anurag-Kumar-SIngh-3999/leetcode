2991f54fd31c7b0c084ae46e22de3c9f659cc333
Anurag-Kumar-SIngh-3999 <anuragks3999@gmail.com>
2026-09-12 17:24:38 +0530

    Time: 13 ms (99.83%), Space: 42.4 MB (17.27%) - LeetHub

--- 0278-first-bad-version/0278-first-bad-version.java
+++ 0278-first-bad-version/0278-first-bad-version.java
@@ -5,19 +5,17 @@ public class Solution extends VersionControl {
     public int firstBadVersion(int n) {
         int low=1;
         int high=n;
-        int bad=n;
-        while(low<=high){
+        while(low < high){
 
             int mid = low + ( high - low )/2;
             if(isBadVersion(mid)){
-                bad=mid;
-                high=mid-1;
+                high=mid;
             }
             else{
                 low=mid+1;
             }
             
         }
-        return bad;
+        return low;
     }
 }
